c348f495f63ec4832f919dddb8b52c1331e5dba8
vadymovychvadym@gmail.com <vadymovychvadym@gmail.com>
2019-04-27 17:11:47 +0300

    adssdas

--- ConsoleApp24/Program.cs
+++ ConsoleApp24/Program.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
 namespace ConsoleApp24
 {
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+               
+                UdpClient uClient = new UdpClient(1024);
+                IPEndPoint ipEnd = null;
+               
+                byte[] responce = uClient.Receive(ref ipEnd);
+                MemoryStream ms = new MemoryStream(responce);
+                Image im = Image.FromStream(ms);
+                im.Save("test" + DateTime.Now.Second.ToString() + ".jpg", ImageFormat.Jpeg);
+                uClient.Close();
+            }
         }
     }
 }
